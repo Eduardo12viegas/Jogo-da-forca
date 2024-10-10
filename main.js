da909d032855a1bd023f4dbe7e250fb24c786b63
Eduardo.viegas <Eduardo.viegas@etec.sp.gov.br>
2024-10-10 12:24:43 -0300

    Iniciado o progeto

---
+++ main.js
@@ -0,0 +1,5 @@
+const palavras = ["programacao", "pneumoultramicroscopicossilicovulcanoconiotico"];
+let tentativasUsadas = 0;
+let letras = ['A','B','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',];
+let palavraSecreta, palavraOculta;
+let jogando;
